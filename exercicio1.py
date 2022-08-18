2099ef27a40d871de9ac5a7d6ba4744badf150d4
Volnei Foss <volneifoss55@rede.ulbra.br>
2022-08-18 13:01:47 -0300

    mi first commit

---
+++ exercicio1.py
@@ -0,0 +1,18 @@
+#Faça um algoritmo que resolva as seguintes expressoes aritmeticas considerando A=2, B=5 C=10.
+#Mostre o resultado na tela da expressao
+# a. A+B*C/A
+# b. (A+B)*C/A
+# c. (A+B*C)/A
+
+a = 2
+b = 5
+c = 10
+
+totalA = a+b*c/a
+print(totalA)
+
+totalB = (a+b)*c/a
+print(totalB)
+
+totalC = (a+b*c)/a
+print(totalC)
