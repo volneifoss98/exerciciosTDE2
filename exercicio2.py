1042e315f248ccedb62aa4dfe550a4e9c5e47021
Volnei Foss <volneifoss55@rede.ulbra.br>
2022-08-18 13:12:25 -0300

    finalizado

---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+#Faça um algoritmo que leia dois números reais e imprima a soma e a media aritmetica desses números.
+  
+valor1 = int(input('digite um numero: '))
+valor2 = int(input('digite outro numero: '))
+
+total = valor1 + valor2
+print(total)
+
+media = total/2
+print(media)
