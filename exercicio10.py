70e58c7654e96f2b39b6e88c9e211f37380f179f
Volnei Foss <volneifoss55@rede.ulbra.br>
2022-08-18 23:38:51 -0300

    finalizado

---
+++ exercicio10.py
@@ -0,0 +1,4 @@
+preco = int(input('Digite o preço do produto: R$ '))
+quantidade = int(input('Digite a quantidade desejada do produto: '))
+total = quantidade * preco 
+print(f"O seu total ficou em R${total}")
---
+++ exercicio11.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite outro numero para saber o resto da divisao: '))
+restoDivisao = numero1%numero2
+
+print(f" o resto da divisao do número {numero1} pelo número {numero2} é: {restoDivisao}")
