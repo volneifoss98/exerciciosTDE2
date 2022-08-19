3db87af8f026c56ee2b12461e2d50245ca6f8c79
Volnei Foss <volneifoss55@rede.ulbra.br>
2022-08-19 00:05:55 -0300

    finalizado

---
+++ exercicio12.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Digite um numero: '))
+numero2 = int(input('Digite outro numero para saber o valor da divisao: '))
+
+resultado = numero1//numero2
+print(f"o valor inteiro da divisao do número {numero1} pelo número {numero2} é: {resultado}")
